4285941884220da0fd2c786d54713490d9c2971b
Wladyslaw Hawrylczak <wladyslaw.hawrylczak@allegro.pl>
2015-10-26 15:46:53 +0100

    config changes

--- nico-reactive-server/src/main/java/mh/poc/nico/reactive/server/ServerApp.java
+++ nico-reactive-server/src/main/java/mh/poc/nico/reactive/server/ServerApp.java
@@ -1,22 +1,10 @@
 package mh.poc.nico.reactive.server;
 
-import com.netflix.nicobar.core.archive.ModuleId;
-import com.netflix.nicobar.core.execution.HystrixScriptModuleExecutor;
-import com.netflix.nicobar.core.execution.ScriptModuleExecutable;
-import com.netflix.nicobar.core.module.ScriptModule;
-import com.netflix.nicobar.core.module.ScriptModuleLoader;
-import com.netflix.nicobar.core.module.ScriptModuleUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.reactive.web.http.HttpServer;
-import org.springframework.stereotype.Component;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Callable;
 
 
 @SpringBootApplication
